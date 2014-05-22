080c735892c2227b75ff4b4727febd15d7fe577b
Arslan Kirmani <arslankirmani9600@gmail.com>
2014-05-22 05:16:09 -0700

    Remove dalvik.vm.dexopt-flags
    Explanation:
    * v=a (verify all classes) is default behaviour
    * o=v (optimize verified classes) is default behaviour
    * m=y (generate register maps) is not default, but it also enables
    precise GC, which increases .dex size.
    * u=y is no longer parsed. Uni or SMP support is set during compile.
    
    The only option that does anything (m=y) is supposed to be activated
    via PRODUCT_TAGS += dalvik.gc.type-precise - but we don't want this.
    
    Thanks  psyke83

--- pico.mk
+++ pico.mk
@@ -241,7 +241,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.purgeable_assets=1 \
     ro.telephony.call_ring.delay=3000 \
     ro.config.low_ram=true \
-    dalvik.vm.dexopt-flags=v=a,o=v,m=y,u=y \
     dalvik.vm.heapstartsize=5m \
     dalvik.vm.heapgrowthlimit=48m \
     dalvik.vm.heapsize=64m \
