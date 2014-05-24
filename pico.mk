a80e81f19e536d4430fa523d8929997b4d05b491
Sachin Thomas <sachinthomas@bitbucket.org>
2014-05-24 18:32:55 +0530

    Add DeviceSettings

--- pico.mk
+++ pico.mk
@@ -211,6 +211,10 @@ PRODUCT_COPY_FILES += \
     device/htc/pico/prebuilt/bin/htc_ebdlogd:system/bin/htc_ebdlogd \
     device/htc/pico/prebuilt/bin/logcat2:system/bin/logcat2 \
 
+# DeviceSettings
+PRODUCT_PACKAGES += \
+    DeviceSettings	
+
 # Keylayouts
 PRODUCT_COPY_FILES += \
     device/htc/pico/prebuilt/usr/keychars/qwerty2.kcm.bin:system/usr/keychars/qwerty2.kcm.bin \
