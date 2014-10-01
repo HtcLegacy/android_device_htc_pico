8f92328f28c78c20a9848704e606c28ad358aef8
Sachin Thomas <sachinthomas@bitbucket.org>
2014-10-01 15:36:00 +0530

    Fix MTP mounting (thewisenerd)
    https://github.com/CyanogenMod/android_build/blob/cm-11.0/tools/post_process_props.py#L34

--- pico.mk
+++ pico.mk
@@ -146,7 +146,7 @@ PRODUCT_COPY_FILES += \
     
 # Set usb type
 ADDITIONAL_DEFAULT_PROPERTIES += \
-    persist.sys.usb.config=mtp,adb \
+    persist.sys.usb.config=mtp \
     persist.service.adb.enable=1 \
     ro.adb.secure=0
 
