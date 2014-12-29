a4c23ae8f424a5d4041a6acfc1d7d743995cd7a7
kirmaniarslan <arslankirmani9600@gmail.com>
2014-12-29 20:17:30 +0100

    pico : Disable JIT
    Change-Id: I8eb0b0895182f16e593c55a2a26f85de884f75a1

--- pico.mk
+++ pico.mk
@@ -280,15 +280,19 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.purgeable_assets=1 \
     ro.telephony.call_ring.delay=3000 \
     ro.config.low_ram=true \
-    dalvik.vm.heapstartsize=5m \
+    ro.com.android.mobiledata=false \
+    ro.com.android.dataroaming=false
+
+# Dalvik Tweaks	
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.jit.codecachesize=0 \
+	dalvik.vm.heapstartsize=5m \
     dalvik.vm.heapgrowthlimit=48m \
     dalvik.vm.heapsize=64m \
     dalvik.vm.heaptargetutilization=0.75 \
     dalvik.vm.heapminfree=512k \
-    dalvik.vm.heapmaxfree=2m \
-    ro.com.android.mobiledata=false \
-    ro.com.android.dataroaming=false
-
+    dalvik.vm.heapmaxfree=2m
+	
 # Disable atlas services on low-ram targets
 PRODUCT_PROPERTY_OVERRIDES += \
     config.disable_atlas=true	
