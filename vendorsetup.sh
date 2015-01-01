9e7aed3b45ade8c4a99c902b1703afe18cc8144e
kirmaniarslan <arslankirmani9600@gmail.com>
2015-01-01 12:14:40 +0100

    omni-5.0 bringup

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,4 +20,5 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-add_lunch_combo cm_pico-userdebug
+add_lunch_combo omni_pico-eng
+add_lunch_combo omni_pico-userdebug
