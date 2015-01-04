378b8091258a3782ae1596d3e2ede66ecb498b20
Arslan Kirmani <arslankirmani9600@gmail.com>
2015-01-04 13:47:17 -0800

    pico : update cm.mk
    - this is htc device not google so override it
    - automatic versioning of builds

--- cm.mk
+++ cm.mk
@@ -12,6 +12,7 @@ $(call inherit-product, device/common/gps/gps_eu_supl.mk)
 $(call inherit-product, device/htc/pico/pico.mk)
 
 PRODUCT_RELEASE_NAME := Explorer
+PRODUCT_GMS_CLIENTID_BASE := android-htc-rev
 
 # Product name
 PRODUCT_NAME := cm_pico
@@ -19,5 +20,7 @@ PRODUCT_BRAND := HTC
 PRODUCT_DEVICE := pico
 PRODUCT_MODEL := Explorer A310e
 PRODUCT_MANUFACTURER := HTC
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=pico BUILD_ID=LRX22C BUILD_FINGERPRINT=htc_europe/htc_pico/pico:5.0.1/LRX22C/87995:user/release-keys
-PRIVATE_BUILD_DESC="1.28.401.1 CL207463 release-keys"
+PRODUCT_CHARACTERISTICS := phone
+
+#PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=pico BUILD_ID=LRX22C BUILD_FINGERPRINT=htc_europe/htc_pico/pico:5.0.1/LRX22C/87995:user/release-keys
+#PRIVATE_BUILD_DESC="1.28.401.1 CL207463 release-keys"
